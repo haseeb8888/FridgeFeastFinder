a501d9e1f59ab2d46edd9cbfa43b65c4b667238e
coderkat <katherineasdavis@gmail.com>
2023-11-27 22:16:44 -0500

    created IngredientSelectionView and IngredientSelectionViewController screen

---
+++ FridgeFeastFinder/IngredientSelectionScreen/IngredientSelection.swift
@@ -0,0 +1,20 @@
+//
+//  IngredientSelection.swift
+//  FridgeFeastFinder
+//
+//  Created by Katherine Davis on 2023-11-27.
+//
+
+import UIKit
+
+class IngredientSelection: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
