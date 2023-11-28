2d6914c9f8f3397b055fb325d611f371f136c305
coderkat <katherineasdavis@gmail.com>
2023-11-27 21:49:26 -0500

    creating files for DisplayRecipe screen (group, view and viewcontroller)

---
+++ FridgeFeastFinder/DisplayRecipeScreen/DisplayRecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  DisplayRecipeView.swift
+//  FridgeFeastFinder
+//
+//  Created by Katherine Davis on 2023-11-27.
+//
+
+import UIKit
+
+class DisplayRecipeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
