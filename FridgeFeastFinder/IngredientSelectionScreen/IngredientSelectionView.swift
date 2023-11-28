eb6b2c52fe62689bd7906fb7c134ee639cd951e1
Ami Yokoyama <ami.yokoyama1@gmail.com>
2023-11-28 14:29:31 -0800

    search function

---
+++ FridgeFeastFinder/IngredientSelectionScreen/IngredientSelectionView.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientSelectionView.swift
+//  FridgeFeastFinder
+//
+//  Created by Katherine Davis on 2023-11-27.
+//
+
+import UIKit
+
+class IngredientSelectionView: UIView {
+    
+
+}
--- FridgeFeastFinder/IngredientSelectionScreen/IngredientSelectionViewController.swift
+++ FridgeFeastFinder/IngredientSelectionScreen/IngredientSelectionViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class IngredientSelectionViewController: UIViewController {
 
+    let ingredientSelectionView = IngredientSelectionView()
+    
+    override func loadView() {
+        view = ingredientSelectionView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "What's in your fridge?"
+        
+        //MARK: reset button definition
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Reset",
+            image: UIImage(systemName: "eraser"),
+            target: self,
+            action: #selector(onResetBarButtonTapped)
+        )
+    }
+    
+    @objc func onResetBarButtonTapped(){
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
