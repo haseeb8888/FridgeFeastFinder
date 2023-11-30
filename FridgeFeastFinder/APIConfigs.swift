d274ea7d59da830eb8846d58ab1f0c69c9044acb
Ami Yokoyama <ami.yokoyama1@gmail.com>
2023-11-30 00:06:01 -0800

    API

---
+++ FridgeFeastFinder/APIConfigs.swift
@@ -0,0 +1,12 @@
+//
+//  APIConfigs.swift
+//  FridgeFeastFinder
+//
+//  Created by Ami Yokoyama on 11/30/23.
+//
+
+import Foundation
+class APIConfigs{
+    //MARK: API base URL
+    static let baseURL = "https://api.spoonacular.com"
+}
