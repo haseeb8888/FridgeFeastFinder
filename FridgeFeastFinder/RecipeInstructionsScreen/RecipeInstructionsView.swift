ac1bb427726be5bcf9675f8e8134dcadf28873a3
coderkat <katherineasdavis@gmail.com>
2023-11-30 00:19:54 -0500

    added back button functionality to the recipe instructions display screen (view and viewcontroller files updated)

--- FridgeFeastFinder/RecipeInstructionsScreen/RecipeInstructionsView.swift
+++ FridgeFeastFinder/RecipeInstructionsScreen/RecipeInstructionsView.swift
@@ -11,12 +11,40 @@ class RecipeInstructionsView: UIView {
     
     // when a cell is tapped in the table on the  display recipes screen, we navigate to this page with the instructions for the recipe.
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var backButton: UIButton!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        //MARK: initializing a TableView...
+        setupBackButton()
+        initConstraints()
     }
-    */
+    
+    func setupBackButton() {
+        backButton = UIButton(type: .system)
+        backButton.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        backButton.setTitle("Back", for: .normal)
+        backButton.setTitleColor(.white, for: .normal)
+        backButton.backgroundColor = .black
+        backButton.layer.cornerRadius = 8
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(backButton)
+    }
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            backButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            backButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            // backButton.topAnchor.constraint(equalTo: tableViewRecipes.bottomAnchor, constant: 40),
+            backButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
 }
--- FridgeFeastFinder/RecipeInstructionsScreen/RecipeInstructionsViewController.swift
+++ FridgeFeastFinder/RecipeInstructionsScreen/RecipeInstructionsViewController.swift
@@ -18,19 +18,14 @@ class RecipeInstructionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        recipeInstructionsScreen.backButton.addTarget(self, action: #selector(onBackButtonTapped), for: .touchUpInside)
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func onBackButtonTapped(){
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }
