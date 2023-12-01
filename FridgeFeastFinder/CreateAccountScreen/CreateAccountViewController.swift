d080d4eb4b4c9ef780dd41c7ef206b1e0fb2a097
Ami Yokoyama <ami.yokoyama1@gmail.com>
2023-12-01 09:59:34 -0800

    added hideKeyboard in CreateAccountViewController

--- FridgeFeastFinder/CreateAccountScreen/CreateAccountViewController.swift
+++ FridgeFeastFinder/CreateAccountScreen/CreateAccountViewController.swift
@@ -18,11 +18,26 @@ class CreateAccountViewController: UIViewController {
         view = createAccountView
         createAccountView.createAccButton.addTarget(self, action: #selector(onCreateAccTapped), for: .touchUpInside)
 
+        hideKeyboardOnTapOutside()
     }
     
     @objc func onCreateAccTapped(){
         //MARK: creating a new user on Firebase...
         createNewAccount()
+    }
+    
+    //MARK: hide keyboard logic...
+    func hideKeyboardOnTapOutside(){
+        //MARK: recognizing the taps on the app screen, not the keyboard...
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardOnTap))
+        tapRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    //MARK: Hide Keyboard...
+    @objc func hideKeyboardOnTap(){
+        //MARK: removing the keyboard from screen...
+        view.endEditing(true)
     }
     
 
