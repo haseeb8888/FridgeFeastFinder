771c6ca906c47538f1aa5a52cfa7c1dc721f690c
coderkat <katherineasdavis@gmail.com>
2023-11-30 22:53:14 -0500

    created a swift file for "Ingredient" and beginning to write code for the get request in the IngredientSelectionViewController

---
+++ FridgeFeastFinder/Data Models/Ingredient.swift
@@ -0,0 +1,12 @@
+//
+//  Ingredient.swift
+//  FridgeFeastFinder
+//
+//  Created by Katherine Davis on 2023-11-30.
+//
+
+import Foundation
+
+struct Ingredient: Codable {
+    let ingredient: String
+}
--- FridgeFeastFinder/IngredientSelectionScreen/IngredientSelectionViewController.swift
+++ FridgeFeastFinder/IngredientSelectionScreen/IngredientSelectionViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class IngredientSelectionViewController: UIViewController, UITextFieldDelegate {
     
@@ -15,6 +16,7 @@ class IngredientSelectionViewController: UIViewController, UITextFieldDelegate {
     override func loadView() {
         view = ingredientSelectionView
         ingredientSelectionView.findRecipesButton.addTarget(self, action: #selector(onFindRecipesButtonTapped), for: .touchUpInside)
+        // so that user input for ingredient is captured and we can delegate to the display recipes screen
         ingredientSelectionView.textFieldSearch.delegate = self
     }
     
@@ -29,6 +31,57 @@ class IngredientSelectionViewController: UIViewController, UITextFieldDelegate {
             target: self,
             action: #selector(onResetBarButtonTapped)
         )
+    }
+
+    
+    //MARK: add a new contact call: add endpoint...
+    func addANewIngredient(ingredient: Ingredient){
+        let endpoint = "/food/ingredients/autocomplete"
+        if let url = URL(string: APIConfigs.baseURL + endpoint){
+            
+            AF.request(url, method:.get, parameters:
+                        [
+                            "ingredent": ingredient.ingredient
+                        ])
+                .responseString(completionHandler: { response in
+                    //MARK: retrieving the status code...
+                    let status = response.response?.statusCode
+                    
+                    switch response.result{
+                    case .success(let data):
+                        //MARK: there was no network error...
+                        
+                        //MARK: status code is Optional, so unwrapping it...
+                        if let uwStatusCode = status{
+                            switch uwStatusCode{
+                                case 200...299:
+                                //MARK: the request was valid 200-level...
+                                
+                                    break
+                        
+                                case 400...499:
+                                //MARK: the request was not valid 400-level...
+                                    print(data)
+                                    break
+                        
+                                default:
+                                //MARK: probably a 500-level error...
+                                    print(data)
+                                    break
+                        
+                            }
+                        }
+                        break
+                        
+                    case .failure(let error):
+                        //MARK: there was a network error...
+                        print(error)
+                        break
+                    }
+                })
+        }else{
+            //alert that the URL is invalid...
+        }
     }
     
     @objc func onResetBarButtonTapped(){
